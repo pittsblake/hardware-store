246244bcf3efee5281d472eea1dc285bfdba96ce
pittsblake <pittsblake1@gmail.com>
2017-10-09 11:09:29 -0400

    added functionality to add to cart button so that cart list appears

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Shop from './Shop'
+
+class HomePage extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <hr />
+        <Shop />
+      </div>
+    )
+  }
+}
+
+export default HomePage
